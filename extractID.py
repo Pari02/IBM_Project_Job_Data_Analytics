8339a3ba37b74e1548ba7c5a490f15a9d0ded73d
Pari <parikshita02@gmail.com>
2016-09-18 16:17:47 -0500

    Update on extractID

--- extractID.py
+++ extractID.py
@@ -9,7 +9,7 @@ Created on Sun Sep 18 08:03:38 2016
 
 # import packages
 import pandas as pd
-import os
+import os, numpy as np
 
 # create function to get #id from the csv file
 def extractID(filename):
@@ -19,13 +19,16 @@ def extractID(filename):
     id_data = file_data["id"]
     # define an empty array to store #id later
     id_num = []
-    # loop to split each id_data 
-    # such that it is broken into two parts 
+    # loop to split each id_data such that it is broken into two parts 
     # first half in head and leaving #id in tail
     for i in id_data:
         head, tail = os.path.split(i)
-        # appending #id to array created earlier
-        id_num.append(tail)        
+        tail = np.int(tail)
+        # appending id number to array created earlier
+        #for x in tail:
+        id_num.append(tail)
+    # convert the array to a list
+    id_num = list(set(id_num))
+    id_num.sort()
+    id_num = [np.str(i) for i in id_num]
     return id_num
-        
-    
